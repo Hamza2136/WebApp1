4180e7b25cf82900bc5ea3f52f1607fa8c0e656c
Hamza <hamzaabid208@gmail.com>
2023-11-29 21:52:59 +0500

    Working Software on 29 11 2023

---
+++ MyWebApp/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MyApp.DataAccessLayer.Infrastructure.IRepository;
+using MyApp.Models;
+using System.Security.Claims;
+
+namespace MyWebApp.Areas.Admin.Controllers
+{
+    [Authorize]
+    [Area("Admin")]
+    public class OrderController : Controller
+    {
+        private IUnitOfWork _unitofwork;
+
+        public OrderController(IUnitOfWork unitofwork)
+        {
+            _unitofwork = unitofwork;
+        }
+
+        #region APICALL
+        public IActionResult AllOrders()
+        {
+            IEnumerable<OrderHeader> orderheader;
+            if(User.IsInRole("Admin") || User.IsInRole("Employee"))
+            {
+                orderheader = _unitofwork.OrderHeader.GetAll(IncludeProperties: "ApplicationUser");
+            }
+            else
+            {
+                var claimsidentity = (ClaimsIdentity)User.Identity;
+                var claims = claimsidentity.FindFirst(ClaimTypes.NameIdentifier);
+                orderheader = _unitofwork.OrderHeader.GetAll(x=> x.ApplicationUserId == claims.Value);
+            }
+            return Json(new { data = orderheader });
+        }
+        #endregion
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MyWebApp/wwwroot/js/AllOrder.js
@@ -0,0 +1,22 @@
+﻿var dtable;
+$(document).ready(function () {
+    dtable = $('#mytable').DataTable({
+        "ajax": {
+            "url": "/Admin/Order/AllOrders"
+        },
+        "columns": [
+            { "data": 'name' },
+            { "data": 'phone' },
+            { "data": 'orderStatus' },
+            { "data": 'orderTotal' },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `
+                    <a href="/admin/order/orderdetails?id=${data}"><i class="bi bi-pencil-square"></i></a>
+                    `;
+                }
+            }
+        ]
+    })
+})
